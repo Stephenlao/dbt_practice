61f92436153a3a9253e40b9371e3cd2d99064f8d
Stephenlao <laovinhkhang2002@gmail.com>
2026-07-15 18:47:04 +0700

    Modify dbt test from models sources

--- Project_Sales/macros/test/all_columns_null.sql
+++ Project_Sales/macros/test/all_columns_null.sql
@@ -1,17 +1,24 @@
 {% test all_columns_null(model, exclude_column_list) %}
 {%- if execute -%}
-{%- set model_columns = adapter.get_columns_in_relation(model) -%}
+    {# dbt automatically passes the source relation into the 'model' variable #}
+    {%- set source_columns = adapter.get_columns_in_relation(model) -%}
 
-{%- for col in model_columns if col.column|upper not in exclude_column_list|upper %}
-    select count(*) as c from (
-        select `{{ col.column }}`
-        from {{ model }}
-        where `{{ col.column }}` is null
-    )
-    having c > 0
-    {%- if not loop.last %}
-    union all
-    {%- endif %}
-{%- endfor -%}
+    {# Ensure the exclusion list items are uppercase for consistent comparison #}
+    {%- set excluded_upper = [] -%}
+    {%- for col in exclude_column_list -%}
+        {%- do excluded_upper.append(col | upper) -%}
+    {%- endfor -%}
+
+    {%- for col in source_columns if col.column | upper not in excluded_upper %}
+        select count(*) as c from (
+            select `{{ col.column }}`
+            from {{ model }}
+            where `{{ col.column }}` is null
+        )
+        having c > 0
+        {%- if not loop.last %}
+        union all
+        {%- endif %}
+    {%- endfor -%}
 {%- endif -%}
 {% endtest %}
